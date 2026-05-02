0fbaaafcde4a2ec5bbf37600ed2dd5a747216513
glrleonel <glrleonel@gmail.com>
2026-05-01 22:12:22 -0300

    Create get-commands.js

---
+++ api/get-commands.js
@@ -0,0 +1,26 @@
+module.exports = async function handler(req, res) {
+  try {
+    const response = await fetch(
+      `${process.env.SUPABASE_URL}/rest/v1/commands?status=eq.pending&order=created_at.asc&limit=5`,
+      {
+        headers: {
+          "apikey": process.env.SUPABASE_SERVICE_ROLE_KEY,
+          "Authorization": `Bearer ${process.env.SUPABASE_SERVICE_ROLE_KEY}`
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    return res.status(200).json({
+      ok: true,
+      data
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      error: error.message
+    });
+  }
+};
